6553347a443656d4aaa59d4146aa7ac7b62c5b37
SunCh1p <blaha.s.connor@gmail.com>
2025-09-06 22:52:36 -0400

    Implemented an axis object, and created a 'scene tree'

--- projects/assignment1/include/composite.h
+++ projects/assignment1/include/composite.h
@@ -1,32 +1,22 @@
 #pragma once
 #include <unordered_map>
+#include <vector>
 #include <memory>
 #include <GL/glut.h>
 
+//TODO create vector3 class
 struct Vector3{
-    float x, y, z;
-
+    GLdouble x, y, z;
+    Vector3(GLdouble X, GLdouble Y, GLdouble Z) : x(X), y(Y), z(Z){}
+    Vector3 operator+(const Vector3& other){
+        return Vector3(x+other.x, y+other.y, z+other.z);
+    }
     bool operator==(Vector3 other){
         return x==other.x&&y==other.y&&z==other.z;
     }
-
     bool operator!=(Vector3 other){
         return !(*this == other);
     }
-};
-
-//Using Composite Design pattern
-//abstract component
-class Component{
-    public:
-        Component(Vector3 position): m_position(position) {}
-        virtual void draw(Vector3 parentPosition) const = 0;
-        virtual void addChild(BodyPart part ,std::unique_ptr<Component> child) = 0;
-
-    protected:
-        //coordinates relative to parent where parent is (0,0,0)
-        Vector3 m_position;
-  
 };
 
 enum class BodyPart{
@@ -39,44 +29,114 @@ enum class BodyPart{
     Default
 };
 
-//composite class
-class HumanoidRobot : public Component{
+//Using Composite Design pattern
+//abstract component used for defining scenes and things that can exist within scenes
+class Component{
     public:
-        void draw(Vector3 parentPosition = {0,0,0}) const override{
-            for(auto& it: children){
-                it.second->draw(m_position);
-            }
-        }
+        Component(Vector3 position = Vector3(0,0,0)): m_position(position) {}
+        //must be overwritten
+        virtual void draw(Vector3 parentPosition) const = 0;
+        //optional to override
+        virtual void addChild(BodyPart part ,std::unique_ptr<Component> child){}
+        virtual void addChild(std::unique_ptr<Component> child){}
 
-        void addChild(BodyPart part ,std::unique_ptr<Component> child) override{
-            //move unique ptr into container
-            children[part] = std::move(child);
-        }
-
-    private:
-        //all children of humanoid robot
-        std::unordered_map<BodyPart, std::unique_ptr<Component>> children;
+    protected:
+        //coordinates relative to parent where parent is (0,0,0)
+        Vector3 m_position;
+  
 };
 
-//leaf classes
-class Sphere : public Component{
+//composite classes
+class Scene : public Component{
     public:
-        void draw(Vector3 parentPosition) const override {
-            //Push a new matrix
-            glPushMatrix();
-            //Apply color
-            glColor3d(1,0,0); //red for now until we get a single object working
-            //Apply translation
+        Scene(Vector3 position = Vector3(0,0,0)): Component(position){}
+        void draw(Vector3 parentPosition = {0,0,0}) const override{
+            //get position in world
+            Vector3 globalPosition = parentPosition + m_position;
+            //draw all m_children relative to current position
+            for(auto& it: m_children){
+                it->draw(globalPosition);
+            }
+        }
+        void addChild(std::unique_ptr<Component> child) override{
+            //move unique ptr into container
+            m_children.push_back(std::move(child));
+        }
+    private:
+        std::vector<std::unique_ptr<Component>> m_children;
+};
 
-
-            //Apply Rotation
-
-
-            //rest to state before function
-            glPopMatrix();
+class HumanoidRobot : public Component{
+    public:
+        HumanoidRobot(Vector3 position = Vector3(0,0,0)): Component(position){}
+        void draw(Vector3 parentPosition = {0,0,0}) const override{
+            //get position in world
+            Vector3 globalPosition = parentPosition + m_position;
+            //draw all m_children relative to current position
+            for(auto& it: m_children){
+                it.second->draw(globalPosition);
+            }
+        }
+        void addChild(BodyPart part ,std::unique_ptr<Component> child) override{
+            //move unique ptr into container
+            m_children[part] = std::move(child);
         }
 
     private:
+        //all m_children of humanoid robot
+        std::unordered_map<BodyPart, std::unique_ptr<Component>> m_children;
+};
 
+//Cube(a leaf class)
+class Cube : public Component{
+    public:
+        Cube(GLdouble size, Vector3 position = Vector3(0,0,0)): m_size(size), Component(position) {}
+        void draw(Vector3 parentPosition) const override{
+            //push a new matrix
+            glPushMatrix();
+                //apply color(Everything is red for now)
+                glColor3d(1,0,0);
+                //set the position
+                Vector3 globalPosition = parentPosition+m_position;
+                glTranslated(globalPosition.x, globalPosition.y, globalPosition.z);
+                //draw the cube(Solid for now)
+                glutWireCube(m_size);
+            //reset to state before function
+            glPopMatrix();
+        }
+    private:
+        GLdouble m_size;
+};
+
+//axes(a leaf class)
+class Axes : public Component{
+    public:
+        Axes(Vector3 position = Vector3(0,0,0)) : Component(position){}
+        void draw(Vector3 parentPosition) const override{
+            //get global position
+            Vector3 globalPosition = parentPosition+m_position;
+            //push a new matrix
+            glPushMatrix();
+            //draw x red relative to parents position
+            glBegin(GL_LINES);
+            glColor3f(1,0,0);
+            glVertex3f(globalPosition.x, globalPosition.y, globalPosition.z);
+            glVertex3f(globalPosition.x+20, globalPosition.y, globalPosition.z);
+            //draw y green
+            glColor3f(0,1,0);
+            glVertex3f(globalPosition.x, globalPosition.y, globalPosition.z);
+            glVertex3f(globalPosition.x, globalPosition.y+20, globalPosition.z);
+            //draw z blue
+            glColor3f(0,0,1);
+            glVertex3f(globalPosition.x, globalPosition.y, globalPosition.z);
+            glVertex3f(globalPosition.x, globalPosition.y, globalPosition.z+20);
+            glEnd();
+            //reset to state before function
+            glPopMatrix();
+        }
+};
+
+//class for camera
+class CameraController{
 
 };
--- projects/assignment1/src/main.cpp
+++ projects/assignment1/src/main.cpp
@@ -1,38 +1,20 @@
 #include <GL/glut.h>
 #include <cstdlib>
 #include <ctime>
+#include "composite.h"
 #define WIN_X 100
 #define WIN_Y 100
 #define WIN_H 720
 #define WIN_W 1280
 
-void DrawAxes(){
-    glBegin(GL_LINES);
-        glColor3f(1,0,0); glVertex3f(0,0,0); glVertex3f(10,0,0);
-        glColor3f(0,1,0); glVertex3f(0,0,0); glVertex3f(0,10,0);
-        glColor3f(0,0,1); glVertex3f(0,0,0); glVertex3f(0,0,10);
-    glEnd();
-}
-
+Scene global_scene(Vector3(0,0,0));
 
 //callback function for glutDisplayFunc
 void myDisplay(){
     glClear(GL_COLOR_BUFFER_BIT);
-    // //set color to white
-    // glColor3f(1.0, 1.0, 1.0);
-    // //draw square which is all white
-    // glBegin(GL_POLYGON);
-    //     //draw vertice 0
-    //     glVertex3f(-0.25, 0.25, 0);
-    //     //draw vertice 1
-    //     glVertex3f(0.25, 0.25, 0);
-    //     //draw vertice 2
-    //     glVertex3f(0.25, -0.25, 0);
-    //     //draw vertice 3
-    //     glVertex3f(-0.25, -0.25, 0);
-    // glEnd();
-    DrawAxes();
-    //end drawing
+    //draw the scene
+    global_scene.draw();
+    //garry.draw();
     glFlush();
 }
 
@@ -41,23 +23,78 @@ void init(void){
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    //glOrtho(-WIN_W/2, WIN_W/2, -WIN_H/2, WIN_H/2, -100, 100);
+    gluPerspective(45.0, (double)WIN_W / WIN_H, 0.1, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(
+        0.0, 2, 10.0,  // camera position
+        0.0, 0.0, 0.0,   // look at origin
+        0.0, 1.0, 0.0    // up vector
+    );
 }
 
+void initScene(){
+    //add axis to the scene
+    global_scene.addChild(std::make_unique<Axes>());
+
+    //Create a humanoid robot named garry
+    auto garry = std::make_unique<HumanoidRobot>();
+    //give garry a head
+    garry->addChild(BodyPart::Head, std::make_unique<Cube>(1.0, Vector3(0, 2.5, 0)));
+    //give garry a torso
+    garry->addChild(BodyPart::Torso, std::make_unique<Cube>(2.0, Vector3(0, 1, 0)));
+    
+    //add garry to the scene
+    global_scene.addChild(std::move(garry));
+}
+
+void procKeys(unsigned char key){
+    switch(key){
+        case 'p':
+            //implementing displaying vertex mode
+            break;
+        case 'w':
+            //implement displaying wireframe mode
+            break;
+        case 's':
+            //implement displaying solid mode
+            break;
+        case 'c':
+            //implement clearing screen and only showing background
+            break;
+        case 'a':
+            //implement toggling axis display
+            break;
+        case 't':
+            //implement speaking functionality
+            break;
+        case 27:
+            exit(0);
+            break;
+    }
+}
 
 int main(int argc, char** argv){
     //initialize glut
     glutInit(&argc, argv);
     //set display mode
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_RGB);
     //set window position
     glutInitWindowPosition(WIN_X, WIN_Y);
     //set window size
     glutInitWindowSize(WIN_W, WIN_H);
     //create the window
     glutCreateWindow("Assignment 1 - Connor Blaha");
+    //Initialize depth checking
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+    glDepthFunc(GL_LEQUAL);
+    glDepthRange(0.0f, 1.0f);
     //initialize opengl state
     init();
+
+    //initialize scene
+    initScene();
     //register callbacks
     glutDisplayFunc(myDisplay); //register display callback
     glutMainLoop(); //enter event loop
